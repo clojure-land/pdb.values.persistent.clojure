25f56941b396ad3529302bfcb8ee9528849f1525
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-02-19 13:59:14 +0100

    Flattening of inheritance hierarchy.

--- src/main/java/org/eclipse/imp/pdb/facts/impl/persistent/clojure/Constructor.java
+++ src/main/java/org/eclipse/imp/pdb/facts/impl/persistent/clojure/Constructor.java
@@ -13,9 +13,12 @@ package org.eclipse.imp.pdb.facts.impl.persistent.clojure;
 
 import static org.eclipse.imp.pdb.facts.impl.persistent.clojure.ClojureHelper.core$merge;
 
+import java.util.Iterator;
 import java.util.Map;
 
 import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.INode;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.exceptions.FactTypeUseException;
 import org.eclipse.imp.pdb.facts.type.Type;
@@ -28,10 +31,15 @@ import clojure.lang.IPersistentVector;
 import clojure.lang.PersistentHashMap;
 import clojure.lang.PersistentVector;
 
-public class Constructor extends Node implements IConstructor {
+public class Constructor extends Value implements IConstructor {
 
+	protected final IPersistentVector children;
+	protected final IPersistentMap annotations;	
+	
 	protected Constructor(Type constructorType, IPersistentVector children, IPersistentMap annotations) {
-		super(constructorType, constructorType.getName(), children, annotations);
+		super(constructorType);
+		this.children = children;
+		this.annotations = annotations;
 	}
 	
 	protected Constructor(Type constructorType) {
@@ -53,12 +61,12 @@ public class Constructor extends Node implements IConstructor {
 
 	@Override
 	public Type getType() {
-		return type.getAbstractDataType();
+		return super.getType().getAbstractDataType();
 	}
 
 	@Override
 	public Type getConstructorType() {
-		return type;
+		return super.getType();
 	}
 
 	@Override
@@ -125,7 +133,7 @@ public class Constructor extends Node implements IConstructor {
 	public boolean equals(Object other) {
 		if (other instanceof Constructor) {
 			Constructor that = (Constructor) other;
-			return this.name.equals(that.name) 
+			return this.getConstructorType().equals(that.getConstructorType()) 
 					&& this.children.equiv(that.children);
 //					&& this.annotations.equiv(that.annotations);
 		} else {
@@ -142,5 +150,60 @@ public class Constructor extends Node implements IConstructor {
 			hash = (hash << 1) ^ (hash >> 1) ^ child.hashCode();
 		}		
 		return hash;
+	}
+
+	@Override
+	public IValue get(int i) throws IndexOutOfBoundsException {
+		return (IValue) children.nth(i);
+	}
+
+	@Override
+	public int arity() {
+		return children.length();
+	}
+
+	@Override
+	public String getName() {
+		return getConstructorType().getName();
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public Iterable<IValue> getChildren() {
+		return (Iterable) children;
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public Iterator<IValue> iterator() {
+		return ((Iterable) children).iterator();
+	}
+
+	@Override
+	public IValue getAnnotation(String label) throws FactTypeUseException {
+		return (IValue) annotations.valAt(label, null);
+	}
+	
+	@Override
+	public boolean hasAnnotation(String label) throws FactTypeUseException {
+		return annotations.containsKey(label);
+	}
+
+	@Override
+	public boolean hasAnnotations() {
+		return !(annotations.count() == 0); 
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Map<String, IValue> getAnnotations() {
+		return (Map<String, IValue>) annotations;
+	}
+
+	@Override
+	public INode replace(int first, int second, int end, IList repl)
+			throws FactTypeUseException, IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
 	}	
 }
