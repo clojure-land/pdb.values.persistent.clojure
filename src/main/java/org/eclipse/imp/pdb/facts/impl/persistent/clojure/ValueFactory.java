dc5fb6b88c221da3bd1f4cec2e6e26f9964d464f
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-07-11 15:26:11 +0200

    Using FastWriter instead of native implement vector based writer.

--- src/main/java/org/eclipse/imp/pdb/facts/impl/persistent/clojure/ValueFactory.java
+++ src/main/java/org/eclipse/imp/pdb/facts/impl/persistent/clojure/ValueFactory.java
@@ -61,27 +61,27 @@ public class ValueFactory extends org.eclipse.imp.pdb.facts.impl.fast.ValueFacto
 	@Override
 	public IListWriter listRelationWriter() {
 //		return new ListWriterWithTypeInference();
-		return new VectorWriterWithTypeInference();
+		return new FastListWriter();
 	}
 
 	@Override
 	public IListWriter listRelationWriter(Type et) {
 //		return new ListWriter(et);
-		return new VectorWriter(et);	
+		return new FastListWriter(et);	
 	}
 
 	@Override
 	public IListWriter listWriter() {
 //		return new ListWriterWithTypeInference();
-		return new VectorWriterWithTypeInference();
-//		return new FastListWriter();
+//		return new VectorWriterWithTypeInference();
+		return new FastListWriter();
 	}
 
 	@Override
 	public IListWriter listWriter(Type et) {
 //		return new ListWriter(et);
-		return new VectorWriter(et);
-//		return new FastListWriter(et);		
+//		return new VectorWriter(et);
+		return new FastListWriter(et);		
 	}
 
 	@Override
